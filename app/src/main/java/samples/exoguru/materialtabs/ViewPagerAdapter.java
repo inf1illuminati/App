efd5de296c3437336d393d6914726b3587aef664
RamonValk <valkramon1@gmail.com>
2015-06-19 12:51:23 +0200

    Aanpassen van de comments

--- app/src/main/java/samples/exoguru/materialtabs/ViewPagerAdapter.java
+++ app/src/main/java/samples/exoguru/materialtabs/ViewPagerAdapter.java
@@ -10,11 +10,11 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  */
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
-    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
-    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+    CharSequence Titles[]; //slaat het aantal tabs op
+    int NumbOfTabs; 
 
 
-    // Build a Constructor and assign the passed Values to appropriate values in the class
+   //constructor
     public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
         super(fm);
 
@@ -23,16 +23,16 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     }
 
-    //This method return the fragment for the every position in the View Pager
+   //returnt fragments van de tabs
     @Override
     public Fragment getItem(int position) {
 
-        if(position == 0) // if the position is 0 we are returning the First tab
+        if(position == 0) 
         {
             Tab1 tab1 = new Tab1();
             return tab1;
         }
-        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        else            
         {
             Tab2 tab2 = new Tab2();
             return tab2;
@@ -41,14 +41,14 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     }
 
-    // This method return the titles for the Tabs in the Tab Strip
+    //returnt de titels
 
     @Override
     public CharSequence getPageTitle(int position) {
         return Titles[position];
     }
 
-    // This method return the Number of tabs for the tabs Strip
+    // returnt aantal tabs
 
     @Override
     public int getCount() {
