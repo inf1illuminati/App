cbeb5db764b933b15589885258a0e527086bce15
RamonValk <valkramon1@gmail.com>
2015-06-19 12:46:57 +0200

    Aanpassen van de comments

--- app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
+++ app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
-    // Declaring Your View and Variables
+    // Declaratie
 
     Toolbar toolbar;
     ViewPager pager;
@@ -27,24 +27,24 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-        // Creating The Toolbar and setting it as the Toolbar for the activity
+        // Aanmaken toolbar
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
 
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        // Aanmaken van ViewPagerAdapter inclusief titels en aantal tabbladen
         adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
 
-        // Assigning ViewPager View and setting the adapter
+        
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(adapter);
 
-        // Assiging the Sliding Tab Layout View
+        
         tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+        tabs.setDistributeEvenly(true); 
 
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        // De kleur van de gui setten
         tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
             public int getIndicatorColor(int position) {
@@ -52,7 +52,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        // Setting the ViewPager For the SlidingTabsLayout
+        
         tabs.setViewPager(pager);
 
 
@@ -69,7 +69,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -81,7 +81,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+       
         if (id == R.id.action_settings) {
             return true;
         }
